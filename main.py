36b60fa721157b436aeb6ba3105e0c54090444fc
KPZNET <kpznet@outlook.com>
2021-08-11 16:45:02 -0600

    McDevs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from StackLinkedList import StackLinkedList
 from QueueLinkedList import QueueLinkedList
+from StackLinkedList import Stack
 
 
 if __name__ == '__main__' :
@@ -44,3 +45,13 @@ if __name__ == '__main__' :
 
     print ( "\nqueue is empty:", queue.isEmpty () )
 
+    print ("Stack List")
+    stack = Stack ()
+    for i in range ( 1, 11 ) :
+        stack.push ( i )
+    print ( f"Stack: {stack}" )
+
+    for _ in range ( 1, 6 ) :
+        remove = stack.pop ()
+        print ( f"Pop: {remove}" )
+    print ( f"Stack: {stack}" )
