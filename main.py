32850079fd12c08a491c516d9c972900191530f6
KPZNET <kpznet@outlook.com>
2021-08-14 11:23:51 -0700

    McDevs

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from Stacks import StackLinkedList
 from Stacks import StackList
 from Queues import QueueLinkedList
 from Queues import QueueList
+import time
 
+NANO_TO_MS = 1000000
 
 class StackRunner:
     def __init__(self):
@@ -11,20 +13,47 @@ class StackRunner:
         self.ls_timing = 0.0
         self.lls_timing = 0.0
 
-    def RunQueueList(self, runs, qu):
-        print("Queue List")
+    def RunQueueList(self, runs, size, qu):
+        print("<<< Queue List RunTimes >>>")
         ll = qu
-        print("Enqueing 5-6-2-10")
-        ll.enqueue ( 5 )
-        ll.enqueue ( 6 )
-        ll.enqueue ( 2 )
-        ll.enqueue ( 10 )
-        print(ll)
 
-        dq = ll.dequeue()
-        print(dq)
+        start_time = time.perf_counter_ns ()
 
+        for k in range(runs):
+            for i in range ( size ) :
+                ll.enqueue ( i )
 
+            for j in range ( size ) :
+                ll.dequeue ()
+
+            if not ll.isEmpty():
+                print("ERROR QUEUE NOT EMPTY")
+
+        stop_time = time.perf_counter_ns ()
+        self.ls_timing = (stop_time - start_time) / NANO_TO_MS
+
+        return  self.ls_timing
+
+    def RunQueueList2(self, runs, size, qu):
+        print("<<< Queue List RunTimes >>>")
+        ll = qu
+
+        start_time = time.perf_counter_ns ()
+
+        for k in range(runs):
+            for i in range ( size/4 ) :
+                ll.enqueue ( i )
+
+            for j in range ( size ) :
+                ll.dequeue ()
+
+            if not ll.isEmpty():
+                print("ERROR QUEUE NOT EMPTY")
+
+        stop_time = time.perf_counter_ns ()
+        self.ls_timing = (stop_time - start_time) / NANO_TO_MS
+
+        return  self.ls_timing
 
 
 
@@ -32,8 +61,13 @@ class StackRunner:
 if __name__ == '__main__' :
 
     sr = StackRunner()
-    sr.RunQueueList(10, QueueList())
-    sr.RunQueueList ( 10, QueueLinkedList () )
+    lq_time = sr.RunQueueList(1000, 2, QueueList())
+    llq_time = sr.RunQueueList ( 10000, 2, QueueLinkedList () )
+    print ( "List Type:{0}  vs. LinkedList Type{1}".format(lq_time, llq_time) )
+
+    lq_time = sr.RunQueueList(2, 10000, QueueList())
+    llq_time = sr.RunQueueList ( 2, 10000, QueueLinkedList () )
+    print ( "List Type:{0}  vs. LinkedList Type{1}".format(lq_time, llq_time) )
 
 
 
