2d53ce7bea3bb27f895bbd259377b9f84f00ff38
Kenneth Ceglia <KPZNet@outlook.com>
2021-08-15 15:06:54 -0700

    AlienDev

--- QueueRunner.py
+++ QueueRunner.py
@@ -1,44 +1,47 @@
 import time
 from matplotlib import pyplot as plt
 
-NANO_TO_MS = 1000000
+NANO_TO_MS = 1000000.0
+AVERAGES = 10
 
 class QueueRunner:
     def __init__(self):
         pass
 
     def RunQueueList(self, runs, size, qu):
-        start_time = time.perf_counter_ns ()
+        ls_timing = 0.0
 
-        for k in range(runs):
-            for i in range ( size ) :
-                qu.enqueue ( i )
+        for r in range(AVERAGES):
+            start_time = time.perf_counter_ns()
+            for k in range(runs):
+                for i in range ( size ) :
+                    qu.enqueue ( i )
 
-            for j in range ( size ) :
-                qu.dequeue ()
+                for j in range ( size ) :
+                    qu.dequeue ()
 
-        stop_time = time.perf_counter_ns ()
-        ls_timing = (stop_time - start_time) / NANO_TO_MS
-
-        return  ls_timing
+            stop_time = time.perf_counter_ns ()
+            ls_timing += ( (stop_time - start_time) / NANO_TO_MS )
+        return  ls_timing / float(AVERAGES)
 
     def RunQueueList2(self, runs, size, deqs, qu):
-        start_time = time.perf_counter_ns ()
+        ls_timing = 0.0
 
-        for i in range ( size ) :
-            qu.enqueue ( i )
+        for i in range(size):
+            qu.enqueue(i)
 
-        for k in range(runs):
-            for i in range ( deqs ) :
-                qu.enqueue ( i )
+        for r in range(AVERAGES):
+            start_time = time.perf_counter_ns ()
+            for k in range(runs):
+                for i in range ( deqs ) :
+                    qu.enqueue ( i )
 
-            for j in range ( deqs ) :
-                qu.dequeue ()
+                for j in range ( deqs ) :
+                    qu.dequeue ()
 
-        stop_time = time.perf_counter_ns ()
-        ls_timing = (stop_time - start_time) / NANO_TO_MS
-
-        return  ls_timing
+            stop_time = time.perf_counter_ns ()
+            ls_timing += ( (stop_time - start_time) / NANO_TO_MS )
+        return  ls_timing / float(AVERAGES)
 
     def plot_queue_comparisons_bg(self, queue_times_df):
         for c in queue_times_df.columns:
--- StackRunner.py
+++ StackRunner.py
@@ -1,44 +1,48 @@
 import time
 from matplotlib import pyplot as plt
 
-NANO_TO_MS = 1000000
+NANO_TO_MS = 1000000.0
+AVERAGES = 10
 
 class StackRunner:
     def __init__(self):
         pass
 
     def RunStackList(self, runs, size, stk):
-        start_time = time.perf_counter_ns ()
+        ls_timing = 0.0
 
-        for k in range(runs):
-            for i in range ( size ) :
-                stk.push ( i )
+        for r in range(AVERAGES):
+            start_time = time.perf_counter_ns ()
+            for k in range(runs):
+                for i in range ( size ) :
+                    stk.push ( i )
 
-            for j in range ( size ) :
-                stk.pop ()
+                for j in range ( size ) :
+                    stk.pop ()
+            stop_time = time.perf_counter_ns ()
+            ls_timing += ( (stop_time - start_time) / NANO_TO_MS )
 
-        stop_time = time.perf_counter_ns ()
-        ls_timing = (stop_time - start_time) / NANO_TO_MS
-
-        return  ls_timing
+        return  ls_timing/float(AVERAGES)
 
     def RunStackList2(self, runs, size, deqs, stk):
-        start_time = time.perf_counter_ns ()
+        ls_timing = 0.0
 
         for i in range ( size ) :
             stk.push ( i )
 
-        for k in range(runs):
-            for i in range ( deqs ) :
-                stk.push ( i )
+        for r in range(AVERAGES):
+            start_time = time.perf_counter_ns()
+            for k in range(runs):
+                for i in range ( deqs ) :
+                    stk.push ( i )
 
-            for j in range ( deqs ) :
-                stk.pop ()
+                for j in range ( deqs ) :
+                    stk.pop ()
 
-        stop_time = time.perf_counter_ns ()
-        ls_timing = (stop_time - start_time) / NANO_TO_MS
+            stop_time = time.perf_counter_ns ()
+            ls_timing += ( (stop_time - start_time) / NANO_TO_MS )
 
-        return  ls_timing
+        return  ls_timing/float(AVERAGES)
 
     def plot_stack_comparisons_bg(self, stack_times_df):
         for c in stack_times_df.columns:
